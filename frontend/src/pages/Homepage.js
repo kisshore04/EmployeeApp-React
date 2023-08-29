a229456b5c33ab64b2401ff760987e2e92cfd7b1
Kisshore <kisshore0423@gmail.com>
2023-08-29 12:48:38 +0530

    added search & sort features

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -45,9 +45,13 @@ import "./../styles/Homepage.css"; // Import your custom styles
 
 function Homepage() {
   const [employees, setEmployees] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortedEmployees, setSortedEmployees] = useState([]);
 
   useEffect(() => {
     getEmployees();
+    // search.toLowerCase()
   }, []);
 
   const getEmployees = async () => {
@@ -60,21 +64,66 @@ function Homepage() {
     }
   };
 
+  const handleSort = () => {
+    const sorted = [...employees].sort((a, b) => {
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+
+      if (sortOrder === "asc") {
+        return nameA.localeCompare(nameB);
+      } else {
+        return nameB.localeCompare(nameA);
+      }
+    });
+
+    // Update the employees state with the sorted array
+    setEmployees(sorted);
+    setSortedEmployees(sorted)
+
+    // Toggle the sort order between ascending and descending
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  };
+
+
   return (
     <div className="homepage-container">
       <div className="home-container">
-        <h3>Employee Details</h3>
         <div>
-          <p>search</p>
+          <h3>Employee Details</h3>
+        </div>
+        <div className="feature-container">
+          <input
+            placeholder="Search employee.."
+            onChange={(e) => setSearch(e.target.value)}
+            className="search-bar"
+          />
+          <button onClick={handleSort} className="sort-button">Sort by Name</button>
         </div>
       </div>
-      {employees.map((employee) => (
+      {/* {employees.filter((employee) => {
+        return search.trim() === ''
+          ? employee
+          : employee.name.toLowerCase().includes(search.toLowerCase());
+      }).map((employee) => (
         <Employee
           employee={employee}
           getEmployees={getEmployees}
           key={employee.id}
         />
-      ))}
+      ))} */}
+      {(search ? sortedEmployees : employees)
+        .filter((employee) => {
+          return search.trim() === ""
+            ? true
+            : employee.name.toLowerCase().includes(search.toLowerCase());
+        })
+        .map((employee) => (
+          <Employee
+            employee={employee}
+            getEmployees={getEmployees}
+            key={employee.id}
+          />
+        ))}
     </div>
   );
 }
